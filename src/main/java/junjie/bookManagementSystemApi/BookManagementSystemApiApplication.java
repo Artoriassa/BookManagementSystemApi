013be989fcbcdf009d107d0494e29635a7367753
Artorias <hjjzxcv123@gmail.com>
2024-04-25 14:28:19 +0800

    init project

---
+++ src/main/java/junjie/bookManagementSystemApi/BookManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package junjie.bookManagementSystemApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagementSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/junjie/bookManagementSystemApi/BookManagementSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package junjie.bookManagementSystemApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
